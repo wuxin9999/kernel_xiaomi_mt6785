1865fa0316c9c0f40df7148d18c29fa718c5e8f0
wuxin9999 <157305210+wuxin9999@users.noreply.github.com>
2024-09-02 17:18:15 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -23,19 +23,6 @@ if [[ $1 = "-t" || $1 = "--tools" ]]; then
 
 	echo 'Setting up toolchain in $(PWD)/toolchain'
 	bash antman -S || exit 1
-
-	echo 'Build libarchive for bsdtar'
-	git clone https://github.com/libarchive/libarchive || true
-	cd libarchive
-	bash build/autogen.sh
-	./configure
-	make -j$(nproc)
-	cd ..
-
-	echo 'Patch for glibc'
-	wget https://gist.githubusercontent.com/itsHanibee/fac63ea2fc0eca7b8d7dcbb7eb678c3b/raw/beacf8f0f71f4e8231eaa36c3e03d2bee9ae3758/patch-for-old-glibc.sh
-	export PATH=$(pwd)/libarchive:$PATH
-	bash patch-for-old-glibc.sh
 fi
 
 # Regenerate defconfig file
